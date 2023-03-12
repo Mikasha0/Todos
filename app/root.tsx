520c05a0dbce8c2f2f12e8e89168e9ecb254484d
Mikasha0 <anikettamrakar0@gmail.com>
2023-03-12 10:22:50 +0545

    added diff style.css for todos

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,11 @@
-import { LiveReload , Outlet} from "@remix-run/react"
+import { LiveReload , Outlet,Links} from "@remix-run/react"
 export default function App() {
   return (
     <html>
       <head>
         <meta charSet="utf-8" />
         <title>Remix Todos App</title>
+        <Links/>
       </head>
       <body>
         <Outlet/>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,11 @@
-export default function Index() {
+import type { LinksFunction } from "@remix-run/node";
+
+import stylesUrl from "~/styles/index.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+export default function IndexRoute() {
   return <div>Hello Index Route</div>;
 }
