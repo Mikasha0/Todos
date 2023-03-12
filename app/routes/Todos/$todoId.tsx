ef56956bca863e1cc40fa120a0c3a30896205935
Mikasha0 <anikettamrakar0@gmail.com>
2023-03-12 10:11:14 +0545

    added a parameterized route

---
+++ app/routes/Todos/$todoId.tsx
@@ -0,0 +1,11 @@
+export default function JokeRoute() {
+    return (
+      <div>
+        <p>This is our parameterized route:</p>
+        <p>
+          Here the todo will appear.
+        </p>
+      </div>
+    );
+  }
+  
