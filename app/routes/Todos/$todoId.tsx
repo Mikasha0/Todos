91c44dc088ff007b9f4440ea84241c322c0603f7
Mikasha0 <anikettamrakar0@gmail.com>
2023-03-13 12:31:32 +0545

    wrapping up todos list

--- app/routes/Todos/$todoId.tsx
+++ app/routes/Todos/$todoId.tsx
@@ -1,11 +1,27 @@
-export default function TodoRoute() {
-    return (
-      <div>
-        <p>This is our parameterized route:</p>
-        <p>
-          Here the todo will appear.
-        </p>
-      </div>
-    );
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  const todo = await db.todo.findUnique({
+    where: { id: params.todoId },
+  });
+  if (!todo) {
+    throw new Error("Joke not found");
   }
-  
+  return json({ todo });
+};
+
+export default function JokeRoute() {
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <div>
+      <p>Here's your hilarious joke:</p>
+      <p>{data.todo.todos}</p>
+      <Link to=".">{data.todo.title} Permalink</Link>
+    </div>
+  );
+}
